f4e09f1c95f74fc6acec79cc35adecc1bdf4a15c
Sidox <pro.sidox@gmail.com>
2022-07-05 15:02:47 +0200

    protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import PrivateRoutes from "./components/privateRoutes/privateRoutes";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/home" element={<Home />} />
+
+      <Route
+        path="/home"
+        element={
+          <PrivateRoutes>
+            <Home />
+          </PrivateRoutes>
+        }
+      />
+      <Route path="*" element={<div>Error 404</div>} />
     </Routes>
   );
 }
