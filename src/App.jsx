6715298543d91529b08f3c971655c5aa91d49e66
micha <michaeldemadet@gmail.com>
2022-07-06 13:36:20 +0200

    add edit slide feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import EditSlide from './components/EditSlide/EditSlide';
+import { database } from './firebaseConfig';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/edit-slide/:id" element={<EditSlide database={database}/>} />
     </Routes>
   );
 }
