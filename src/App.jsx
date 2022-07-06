841fa912979d2512cf448226adca664eef5dc798
Océane Renoux <oceane.renoux@hotmail.fr>
2022-07-06 11:10:45 +0200

    Auto stash before merge of "dev" and "origin/dev"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import ViewSlidePresentation from "./components/ViewSlidePresentation";
 import Presentation from "./components/Presentation";
 import { database } from "./firebaseConfig";
 
 function App() {
   return (
     <Routes>
+      <Route path="/presentation" element={<ViewSlidePresentation />} />
       <Route path="/" element={<Presentation slidesInDb={database} />} />
     </Routes>
   );
